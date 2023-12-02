95ce2c4bde30d4db8acbf068e438e0823fa2f7b6
Paul Snively <psnively@mac.com>
2023-12-02 08:33:07 -0500

    More fun with cats-parse, but apparently still wrong.

--- 2023/scala/src/main/scala/1.2.scala
+++ 2023/scala/src/main/scala/1.2.scala
@@ -1,6 +1,7 @@
 package `1.2023.advent`
 
 import cats._, cats.implicits._
+import cats.data.NonEmptyList
 import cats.effect._, cats.effect.implicits._
 
 import fs2._
@@ -10,8 +11,9 @@ import cats.parse.Parser._
 
 object Part2 extends IOApp.Simple {
   val int   = digit.map(_.asDigit)
+  val joker = string("twone")
   val one   = string("one").as(1)
-  val two   = string("two").as(2)
+  val two   = (string("two") <* not(string("ne"))).as(2)
   val three = string("three").as(3)
   val four  = string("four").as(4)
   val five  = string("five").as(5)
@@ -19,14 +21,18 @@ object Part2 extends IOApp.Simple {
   val seven = string("seven").as(7)
   val eight = string("eight").as(8)
   val nine  = string("nine").as(9)
-  
+  val twtwo = (start ~ alpha.repUntil0(joker) *> joker).as(2).backtrack.map(List(_))
+  val twone = ((joker <* alpha.rep0).as(1)).rep0
+ 
   val number = int | one | two | three | four | five | six | seven | eight | nine
-  val coord = (number.backtrack | alpha.as(0)).rep.map(_.filterNot(_ == 0))
-
+  val tag    = number.surroundedBy(alpha.repUntil0(number)).rep0
+  val coord = ((twtwo | tag) ~ tag ~ twone)
+    .map { case ((a, b), c) => a ++ b ++ c }
+  
   def parseCoordinate(line: String): Either[RuntimeException, Int] = {
     if (line.isBlank()) 0.asRight[RuntimeException] else {
-      val num = coord.parse(line).map { case (_, vals) => vals match {
-        case Nil      => 0
+      val num = coord.parse(line).map { case (_, vs) => vs match {
+        case Nil => 0
         case v :: Nil => v * 10 + v
         case h :: t   => h * 10 + t.last
       }}
